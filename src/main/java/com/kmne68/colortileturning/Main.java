de02140939329f773248e23f36b047da95c6abb5
Keith Emery <kmne68@gmail.com>
2025-05-04 01:32:25 -0500

    Repo cleanup

--- src/main/java/com/kmne68/colortileturning/Main.java
+++ src/main/java/com/kmne68/colortileturning/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.kmne68.colortileturning.game;
+package com.kmne68.colortileturning;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
